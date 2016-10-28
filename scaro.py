30f2c2e251936d8c2cd593a9697af6ec2134c3f9
Paras Avkirkar <dhavalavkirkar6699@gmail.com>
2016-10-28 14:22:10 +0530

    back to python2

--- scaro.py
+++ scaro.py
@@ -30,40 +30,6 @@ count = 0
 problemRows = soup.find_all('tr')
 driver = webdriver.Chrome()
 
-for row in problemRows:
-	problemName = ''
-	# if(count > 2):
-	# 	break
-	if row.has_attr('class') and 'problemrow' in row['class']:
-		allHrefTags = row.find_all('a')
-		for a in allHrefTags:
-			if a.has_attr('href') and 'problem' in a['href']:
-				problemName = a.b.string
-				
-				# driver = webdriver.PhantomJS()
-				# driver = webdriver.Firefox()
-				# firefox_capabilities = DesiredCapabilities.FIREFOX
-				# firefox_capabilities['marionette'] = True
-
-				# driver = webdriver.Firefox(capabilities=firefox_capabilities)
-				# drsiver = webdriver.Chrome()
-				# driver.set_window_size(1120, 550)
-
-				print('inside loop ' + codechefUrl + a['href'])
-				prob = getCodechefProblem(codechefUrl + a['href'], driver)
-				if prob == None:
-					print('issue')
-				else:
-					print('not issue ' + str(count))
-				count = count + 1
-				problemCollections.append(prob)
-
-				
-driver.quit()				
-for problem in problemCollections:
-	print(problem)
-
-
 codechefProblemUrl = 'https://www.codechef.com/problems/'
 sections = ['easy', 'medium', 'hard']
 
@@ -87,7 +53,7 @@ for section in sections[sec_count:]:
 			allHrefTags = row.find_all('a')
 			for a in allHrefTags:
 				if a.has_attr('href') and 'problem' in a['href']:
-					p = getCodechefProblem(codechefUrl+a['href'])
+					p = getCodechefProblem(codechefUrl+a['href'], driver)
 					if p:
 						with open('codechef/'+p.name, 'w+b') as f:
 							pickle.dump(p, f)
