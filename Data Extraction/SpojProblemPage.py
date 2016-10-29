a4650ed2822403bbe3f4ac405a05938e398b3948
Paras Avkirkar <dhavalavkirkar6699@gmail.com>
2016-10-29 10:14:55 +0530

    Rearranged directory structure

--- Data Extraction/SpojProblemPage.py
+++ Data Extraction/SpojProblemPage.py
@@ -29,22 +29,61 @@ def getSpojProblem(problemUrl):
 		driver.get(problemUrl)
 		print('reach problem page')
 
+		#Getting tags of the problem
 		problemTag = ''
 		divTags = driver.find_element_by_id('problem-tags')
 		aTags = divTags.find_elements_by_tag_name('a')
 		for aTag in aTags:
 			problemTag = problemTag + ' ' + aTag.text
 
+
+		#Getting text of the problem
 		problemText = ''
 		divBody = driver.find_element_by_id('problem-body')
 		all_children_by_xpath = divBody.find_elements_by_xpath(".//*")
 		for child in all_children_by_xpath:
 			problemText = problemText + ' ' + child.text
 
-		prob = SpojProblem(problemName, problemUrl, problemTag, problemText)
-		print(problemText)
+		#print(problemText)
 
 		
+		#Getting other infos of the problem
+		addedBy = ''
+		date = ''
+		time = ''
+		source = ''
+		memory = ''
+		cluster = ''
+		languages = ''
+		tableInfo = driver.find_element_by_class_name('probleminfo')
+		allRows = tableInfo.find_elements_by_tag_name('tr')
+		for row in allRows:
+			dataCells = row.find_elements_by_xpath(".//*")
+			label = dataCells[0].text.lower()
+			if('added' in label):
+				addedBy = dataCells[1].text
+			elif('date' in label):
+				date = dataCells[1].text
+			elif('time' in label):
+				time = dataCells[1].text
+			elif('source' in label):
+				source = dataCells[1].text
+			elif('memory' in label):
+				memory = dataCells[1].text
+			elif('cluster' in label):
+				cluster = dataCells[1].text
+			elif('languages' in label):
+				languages = dataCells[1].text					
+
+		print(addedBy)
+		print(date)
+		print(time)
+		print(source)
+		print(memory)
+		print(cluster)
+		print(languages)
+
+		prob = SpojProblem(problemName, problemUrl, problemTag, problemText, addedBy, date, time, source, memory, cluster, languages)
 		return prob
 	except Exception as e:
 			print('element not found')
--- Data Extraction/spojProblem.py
+++ Data Extraction/spojProblem.py
@@ -1,12 +1,20 @@
 class SpojProblem:
 	'Class for representing problems'
 
-	def __init__(self, name, url, tags, description):
+	def __init__(self, name, url, tags, description, addedBy, date, timelimit, sourcelimit, memorylimit, cluster, languages):
 		self.id = id
 		self.name = name
 		self.url = url
 		self.tags = tags
 		self.description = description
+		self.addedBy = addedBy
+		self.date = date
+		self.timelimit = timelimit
+		self.sourcelimit = sourcelimit
+		self.memorylimit = memorylimit
+		self.cluster = cluster
+		self.languages = languages
+
 		#self.process_description()
 
 	def __str__(self):
