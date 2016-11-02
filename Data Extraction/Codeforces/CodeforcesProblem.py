6209ba859c7836f8ebffffa207ae11ed8461f827
Pranay Patil <pranay.patil.dev@gmail.com>
2016-11-02 09:38:03 +0530

    added pickling and file I/O for codeforces_problems

--- Data Extraction/Codeforces/CodeforcesProblem.py
+++ Data Extraction/Codeforces/CodeforcesProblem.py
@@ -10,7 +10,48 @@ class CodeforcesProblem:
 		self.problemStatement = problemStatement
 		self.timelimit = timelimit
 		self.memorylimit = memorylimit
+		self.desription = problemStatement
+		self.process_description()
 
 	def __str__(self):
 		return "Problem name: " + self.name + " Problem tag: " + self.tags + " Problem url: "+self.url
+
+	def process_description(self):
+		description_lines = self.desription.split('\n')
+		print(description_lines)
+		description_lines = [line.strip() for line in description_lines]
+
+		try:
+			input_start = description_lines.index('Input')
+		except ValueError:
+			try:
+				input_start = description_lines.index('Input :')
+			except ValueError:
+				input_start = description_lines.index('Input:')
+
+
+		try:
+			output_start = description_lines.index('Output')
+		except ValueError:
+			try:
+				output_start = description_lines.index('Output :')
+			except ValueError:
+				output_start = description_lines.index('Output:')
+
+		try:
+			example_start = description_lines.index('Examples')
+		except ValueError:
+			try:
+				example_start = description_lines.index('Examples :')
+			except ValueError:
+				example_start = description_lines.index('Examples:')
+
+		self.statement = description_lines[:input_start]
+		self.input = description_lines[input_start:output_start]
+		self.output = description_lines[output_start:example_start]
+		self.constraints = self.input
+		print self.input
+		print self.output
+
+
 	
--- Data Extraction/Codeforces/scrapeCodeforcesProblem.py
+++ Data Extraction/Codeforces/scrapeCodeforcesProblem.py
@@ -1,3 +1,4 @@
+from HTMLParser import HTMLParser
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
@@ -6,18 +7,30 @@ from selenium.webdriver.support import expected_conditions
 from CodeforcesProblemPage import getCodeforcesProblem
 
 import html
+import pickle
 
-count=0
+current_progress = {'page':1, 'problem_no':0}
+
 driver = webdriver.Chrome()
-codeforcesProblemsLink="http://codeforces.com/problemset/page/31"
+try:
+	with open('current_progress.pickle', 'r+b') as f:
+		current_progress = pickle.load(f)
+		print 'Resuming Progress from page '+str(current_progress['page'])+' and problem no '+str(current_progress['problem_no'])
+except Exception as e:
+	print e
+	print 'Starting problem collection'
+
+count = current_progress['problem_no']
+page_no = current_progress['page']
+codeforcesProblemsLink="http://codeforces.com/problemset/page/"
 while True:
-	driver.get(codeforcesProblemsLink)
+	driver.get(codeforcesProblemsLink+str(page_no))
 
 	print("Page successfully loaded")
 
 	problemsTable = driver.find_element_by_class_name("datatable")
 	problemList = problemsTable.find_elements_by_tag_name("tr")
-	for problem in problemList:
+	for problem in problemList[count:]:
 		try:
 			problemLink = problem.find_element_by_class_name("id")
 			problemLink = problemLink.find_element_by_tag_name("a")
@@ -26,23 +39,29 @@ while True:
 			
 			print("Problem link extracted")
 
-			getCodeforcesProblem(problemLink,problemId)
-			count = count + 1
-
-			print("Suceesfully extracted problem " + problemId + "  Count - " + str(count))
+			p = getCodeforcesProblem(problemLink,problemId)
+			if p:
+				with open('codeforces/'+p.name, 'w+b') as f:
+					pickle.dump(p, f)
+				count = count + 1
+				with open('current_progress.pickle', 'w+b') as f:
+					pickle.dump({'page':page_no, 'problem_no':count}, f)
+				print("Suceesfully extracted problem " + problemId + "  Count - " + str(count))
 
 		except Exception as e:
-				print("Table Header")
+			print("Table Header")
 				#print(e)
-	nextPageLink = driver.find_elements_by_class_name("arrow")
-	for link in nextPageLink:
-		if link.text == html.unescape('&rarr;'):
-			nextLink = link.get_attribute("href")
-	if nextLink == codeforcesProblemsLink:
-		break
-	else:
-		codeforcesProblemsLink = nextLink
-
+	# nextPageLink = driver.find_elements_by_class_name("arrow")
+	# for link in nextPageLink:
+	# 	if link.text == HTMLParser().unescape('&rarr;'):
+	# 		nextLink = link.get_attribute("href")
+	# if nextLink == codeforcesProblemsLink:
+	# 	break
+	# else:
+	# 	codeforcesProblemsLink = nextLink
+	page_no += 1
+	print codeforcesProblemsLink + str(page_no)
+	count = 0
 	print("Loading next page")
 
 print("Extraction completed successfully " + str(count))
