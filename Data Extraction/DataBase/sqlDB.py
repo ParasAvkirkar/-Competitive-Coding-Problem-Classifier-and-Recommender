c8ea5bcabf942eaf5c5c4fd690e608952a46efb9
Pranay Patil <pranay.patil.dev@gmail.com>
2016-11-27 11:59:21 +0530

    added db scripts and config file

---
+++ Data Extraction/DataBase/sqlDB.py
@@ -0,0 +1,61 @@
+import MySQLdb
+
+def connect_db():
+		# Open database connection
+		try:
+			with open("dbINFO.txt", "r") as f:
+				ip, username, password, db_name = f.read().split('\n')
+		except:
+			ip = "localhost"
+			username = "root"
+			password = ""
+			db_name = "data stage fyp"
+
+		db = MySQLdb.connect(ip, username, password, db_name)
+		return db
+
+def insert_user_db(tableName, uname, country, userCity, isStudent, problemsSolved, prefLang, rating, rank):
+		if tableName == 'codechef_user':
+			q = "INSERT INTO "+ tableName +" VALUES ( " + "null, '" + uname + "', '" + country + "', '" + userCity + "', '" \
+				+ str(isStudent) + "', '" + prefLang + "', '" + str(rating['Long']) + "', '" + str(rating['Short']) +"', '" + str(rating['LTime']) +"', '" + str(rank['Long']) +"', '"+ str(rank['Short']) +"', '" + str(rank['LTime']) + "');\n"
+		elif tableName == 'spoj_user' or tableName == 'codeforces_user':
+			q = "INSERT INTO "+ tableName +" VALUES ( " + "null, '" + uname + "', '" + country + "', '" + userCity + "', '" \
+				+ str(isStudent) + "', '" + prefLang + "', '" + str(rating) + "', '" + str(rating) +"', '" + str(rating) +"', '" + str(rank) +"', '"+ str(rank) +"', '" + str(rank) + "');\n"
+
+		db = connect_db()
+		# prepare a cursor object using cursor() method
+		cursor = db.cursor()
+
+		# execute SQL query using execute() method.
+		if cursor.execute(q) == 1:
+			db.commit()
+			print "Success DB"
+		else: print "Fail DB"
+
+		for probCode in problemsSolved:
+			if len(probCode)>0:
+				q = "INSERT INTO "+tableName.split("_")[0]+"_prob_user_map VALUES (null, '"+uname+"', '"+probCode+"' )"
+				if cursor.execute(q) == 1:
+					db.commit()
+					print "Success DB"
+				else: print "Fail DB"
+
+
+def insert_problem_db(tableName, prob_code, url, desc, tag, diff, category, sub_size, constraint, time_limit, source_limit, is_example_given):
+		q = "INSERT INTO "+tableName+" VALUES(NULL, '"+(prob_code)+"', '"+str(url)+"', '"+(desc)+"', '"+(tag)+"', '"+str(sub_size)+"', " \
+			"'"+(constraint)+"', '"+str(is_example_given)+"', '"+str(diff)+"', '"+str(category)+"', '"+str(time_limit)+"', '"+str(source_limit)+"') ;"
+
+		# print q
+
+		db = connect_db()
+		db.set_character_set('utf8')
+		# prepare a cursor object using cursor() method
+		cursor = db.cursor()
+		cursor.execute('SET NAMES utf8;')
+		cursor.execute('SET CHARACTER SET utf8;')
+		cursor.execute('SET character_set_connection=utf8;')
+		# execute SQL query using execute() method.
+		if cursor.execute(q) == 1:
+			db.commit()
+			print "Success DB"
+		else: print "Fail DB"
