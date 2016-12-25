02c90dc4800fb1e56fe3069b87144373b0e2f619
Paras Avkirkar <dhavalavkirkar6699@gmail.com>
2016-12-25 11:51:50 +0530

    Unnecessary changes, ignore the commit, forced to do before pull

--- Data Extraction/codechef_User/fetchUserList.py
+++ Data Extraction/codechef_User/fetchUserList.py
@@ -14,9 +14,9 @@ import datetime
 import logging
 
 logging.basicConfig(filename='exceptScenarios.log', level=logging.ERROR)
-f = open('users_ids.txt', 'a')
+f = open('new_users_ids.txt', 'a')
 
-def fetch_user_list(start, driver):
+def fetch_user_list(start, driver, userCount):
 	
 	baseUrl = "https://discuss.codechef.com/users/?sort=karma&page="
 	userLink = baseUrl + str(start)
@@ -33,8 +33,9 @@ def fetch_user_list(start, driver):
 			aas = a.get_attribute('href').split('/')
 			# print aas[5]
 			f.write(aas[5] + '\n')
+			userCount += 1
 			
-	
+		print('Got '+str(userCount))
 	except Exception as e:
 		print('element not found')
 		print(e)
@@ -51,8 +52,8 @@ def fetch_user_list(start, driver):
 		pass
 	
 	
-	if start < 4763:
-		fetch_user_list(start+1, driver)
+	if start < 4940:
+		fetch_user_list(start+1, driver, userCount)
 		
 	
 
@@ -60,7 +61,7 @@ def fetch_user_list(start, driver):
 driver = webdriver.Chrome()
 # fetch_user('anudeep2011', driver)
 # fetch_user('sherlock_holms', driver)
-fetch_user_list(4000, driver)
+fetch_user_list(1, driver, 0)
 # fetch_user('paragpachpute', driver)
 # fetch_user('pranay0007', driver)
 driver.close()
