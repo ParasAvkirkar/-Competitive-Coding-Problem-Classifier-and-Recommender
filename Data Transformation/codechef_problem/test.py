85af1b9d3f37207d13a891b48b389f1ad801fcaf
Paras Avkirkar <dhavalavkirkar6699@gmail.com>
2017-01-09 11:40:10 +0530

    All tag possibility are considered, codeforces tag cleaning done

--- Data Transformation/codechef_problem/test.py
+++ Data Transformation/codechef_problem/test.py
@@ -18,7 +18,33 @@ def get_wordnet_pos(treebank_tag):
     else:
         return ''
 
-s = 'chef likes problems geometry lot please help solve one problem find possible triangles integer sides radius inscribed circle also known incircle equal r two triangles said different least one different side lengths formally let two triangles t1 t2 let b c denote sides triangle t1  b  c similarly let e f denote sides triangle t2  e  f t1 said different t2 either  b  e c  f'
+# s """chef advocate go green initiative 
+#     today n trees planted row outside restaurant today height i-th tree hi 
+#     feet trees grow rate mi feet per day chef knows trees look beautiful form
+#      zig-zag sequence trees said zig-zag sequence heights tree first increases decreases 
+#     alternates decreasing/increasing respectively formally trees said 
+#     zig-zag sequence one following two conditions holds h1 < h2 > h3 < h4 h1 > h2 < h3 > h4 
+#     chef wants know intervals time heights trees form zig-zag sequence"""
+
+#  t = """
+#         submissions problem available read problems statements 
+#         mandarin chinese russian vietnamese well today planned day 
+#         tor thik ayvaks wedding kark infatuated ayvak offers play 
+#         game thik whosoever wins get marry ayvak ayvak values games chance 
+#         things life agrees kark sets n grid (n rows columns) labelled left right 
+#         top bottom consecutively numbers 1 m*n 1 top left corner m*n bottom
+#          right corner example labelled 3 6 grid looks follows: kark already 
+#         painted k unit squares grid heart next thik randomly picks rectangle sides
+#          grid lines positive area valid rectangle equal probability chosen 
+#         three distinct possibilities thiks rectangle 3 6 grid shown below: nine 
+#         different rectangles 2 2 grid shown below: thiks rectangle contains least half hearts 
+#         thik gets marry ayvak otherwise kark marry ayvak kark wants know 
+#         whether advantage wants know expected value number hearts randomly chosen 
+#         rectangle cover im sure good heart please cover job"""   
+s ="chef advocate go green initiative today n trees planted row outside restaurant today height i-th tree hi feet trees grow rate mi feet per day chef knows trees look beautiful form zig-zag sequence trees said zig-zag sequence heights tree first increases decreases alternates decreasing/increasing respectively formally trees said zig-zag sequence one following two conditions holds h1 < h2 > h3 < h4 h1 > h2 < h3 > h4 chef wants know intervals time heights trees form zig-zag sequence"
+t =" submissions problem available read problems statements mandarin chinese russian vietnamese well today planned day tor thik ayvaks wedding kark infatuated ayvak offers play game thik whosoever wins get marry ayvak ayvak values games chance things life agrees kark sets n grid (n rows columns) labelled left right top bottom consecutively numbers 1 m*n 1 top left corner m*n bottom right corner example labelled 3 6 grid looks follows: kark already painted k unit squares grid heart next thik randomly picks rectangle sides grid lines positive area valid rectangle equal probability chosen three distinct possibilities thiks rectangle 3 6 grid shown below: nine different rectangles 2 2 grid shown below: thiks rectangle contains least half hearts thik gets marry ayvak otherwise kark marry ayvak kark wants know whether advantage wants know expected value number hearts randomly chosen rectangle cover im sure good heart please cover job"
+
+s = s + t
 
 newDesc = ''
 mapList = pos_tag(s.split())
