0a7ece1f5af46b19724f01969fa91f8d794f019c
Pranay Patil <pranay.patil.dev@gmail.com>
2017-02-05 17:16:51 +0530

    Added model two training for codeforces problems

--- Model Training/Utilities/constants.py
+++ Model Training/Utilities/constants.py
@@ -1,5 +1,5 @@
 test_size = 0.5
-categories = ['greedy', 'graph', 'tree', 'combinatorics', 'maths', 'segment-tree', 'dp']
+categories = ['greedy', 'graph', 'math', 'dp']
 
 minimum_number_of_probs_inwhich_word_to_exist = 20
 performance_metric_keys = {'precision':0, 'recall':1, 'fscore':2}
