ba6bd287d7a847d1d65b335359e01551a11fdc74
Parag Pachpute <paragpachpute3@gmail.com>
2017-03-28 13:06:08 +0530

    basic web page

--- Server/server.py
+++ Server/server.py
@@ -6,18 +6,18 @@
 #     # result = predict.predict(c)
 #     return jsonify({'Status': str(78)})
 __author__ = 'Pranay'
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, render_template
 
 import  sys
-sys.path.append("../Data Extraction/codechef/")
-sys.path.append("../Data Extraction/Codeforces/")
-sys.path.append("../Data Extraction/SPOJ/")
-import predictCategory, problems, CodeforcesProblem, spojProblem
+# sys.path.append("../Data Extraction/codechef/")
+# sys.path.append("../Data Extraction/Codeforces/")
+# sys.path.append("../Data Extraction/SPOJ/")
+# import predictCategory, problems, CodeforcesProblem, spojProblem
 print 'import complete'
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
    return 'Hello World'
 
 @app.route("/getData", methods=['GET'])
@@ -45,6 +45,34 @@ def postData():
         c = spojProblem.getProblemFromDescription(problem_content)
     result = predictCategory.predict_category(c)
     return jsonify({'result':result})
+
+@app.route("/profile/<name>")
+def profile(name):
+    username = name
+    probsSolved = {"Easy":33, "Medium":5, "Hard":1}
+    categorySolved = {"String":8, "Maths":11, "Dp":5, "Graph":8, "Trees":7}
+    totalSolved = 39
+    recommended_probs = [{"name":"prob1",
+                          "link":"https://www.codechef.com/problems/KOL16F",
+                          "category":"string",
+                          "diff":"medium"},
+                         {"name":"prob2",
+                          "link":"https://www.codechef.com/problems/RSERVER",
+                          "category":"dp",
+                          "diff":"medium"},
+                         {"name":"prob3",
+                          "link":"https://www.codechef.com/problems/KOL1502",
+                          "category":"graph",
+                          "diff":"medium"},
+                         ]
+    user = {
+        "name":username.upper(),
+        "totalSolved":totalSolved,
+        "probsSolved":probsSolved,
+        "categorySolved":categorySolved,
+        "recommended_probs":recommended_probs
+        }
+    return render_template("profile.html", user=user)
 
 # app.run(debug=True)
 if __name__ == '__main__':
